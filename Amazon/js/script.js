ff04c88171dd2c993daf5ea75dc8b9ff25f27f21
BasankoSV <56435380+BasankoSV@users.noreply.github.com>
2021-10-28 17:42:28 +0500

    Delete compile dir

--- Amazon/js/script.js
+++
@@ -1,8 +0,0 @@
-(function (factory) {
-	typeof define === 'function' && define.amd ? define('script', factory) :
-	factory();
-}((function () { 'use strict';
-
-	console.log('Amazon, script.js');
-
-})));
