85863e5508eb5f6de869a4c35f164892309e9eba
BasankoSV <56435380+BasankoSV@users.noreply.github.com>
2021-10-28 17:33:36 +0500

    Add files new project Amaz0n
    Add files new project Amaz0n

---
+++ Amazon/js/script.js
@@ -0,0 +1,8 @@
+(function (factory) {
+	typeof define === 'function' && define.amd ? define('script', factory) :
+	factory();
+}((function () { 'use strict';
+
+	console.log('Amazon, script.js');
+
+})));
