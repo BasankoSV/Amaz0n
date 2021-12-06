aa2460777b31552f85002dc542d0d9bf684d8624
BasankoSV <basankosv@gmail.com>
2021-12-06 16:56:08 +0500

    Changed the direction of movement of the slides

--- Amaz0n/js/script.js
+++ Amaz0n/js/script.js
@@ -19,15 +19,15 @@
       let prev = current;
 
       switch (direction) {
-        case 'right':
-          this.carouselWrapper[current].style = 'transform: translate(120%, 0); transition: transform 1.5s ease 1.5s;';
+        case 'left':
+          this.carouselWrapper[current].style = 'transform: translate(120%, 0); transition: transform 1s ease 1s;';
           current++;
           current === this.carouselWrapper.length ? current = 0 : current;
           this.carouselWrapper[current].style = 'transform: translate(-120%, 0);';
           break;
 
-        case 'left':
-          this.carouselWrapper[current].style = 'transform: translate(-120%, 0); transition: all 1.5s ease 1.5s';
+        case 'right':
+          this.carouselWrapper[current].style = 'transform: translate(-120%, 0); transition: all 1s ease 1s';
           current--;
           current < 0 ? current = this.carouselWrapper.length - 1 : current;
           this.carouselWrapper[current].style = 'transform: translate(120%, 0);';
@@ -35,11 +35,11 @@
       }
 
       setTimeout(() => {
-        this.carouselWrapper[current].style = 'transform: translate(0, 0); transition: all 1.5s ease 1.5s;';
+        this.carouselWrapper[current].style = 'transform: translate(0, 0); transition: all 1s ease 1s;';
       }, 1);
       setTimeout(() => {
         this.carouselWrapper[prev].style = 'transform: translate(0, 0); opacity: 0;';
-      }, 2500);
+      }, 2000);
       this.carouselWrapper[current].setAttribute('data-current', '1');
     }
 
--- src/js/script.js
+++ src/js/script.js
@@ -13,21 +13,21 @@ const carousel = {
     let prev = current
 
     switch (direction) {
-      case 'right':
-        this.carouselWrapper[current].style = 'transform: translate(120%, 0); transition: transform 1.5s ease 1.5s;'
+      case 'left':
+        this.carouselWrapper[current].style = 'transform: translate(120%, 0); transition: transform 1s ease 1s;'
         current++
         (current === this.carouselWrapper.length) ? current = 0 : current
         this.carouselWrapper[current].style = 'transform: translate(-120%, 0);'
         break
-      case 'left':
-        this.carouselWrapper[current].style = 'transform: translate(-120%, 0); transition: all 1.5s ease 1.5s'
+      case 'right':
+        this.carouselWrapper[current].style = 'transform: translate(-120%, 0); transition: all 1s ease 1s'
         current--
         (current < 0) ? current = this.carouselWrapper.length - 1 : current
         this.carouselWrapper[current].style = 'transform: translate(120%, 0);'
         break
     }
-    setTimeout(() => { this.carouselWrapper[current].style = 'transform: translate(0, 0); transition: all 1.5s ease 1.5s;' }, 1)
-    setTimeout(() => { this.carouselWrapper[prev].style = 'transform: translate(0, 0); opacity: 0;' }, 2500)
+    setTimeout(() => { this.carouselWrapper[current].style = 'transform: translate(0, 0); transition: all 1s ease 1s;' }, 1)
+    setTimeout(() => { this.carouselWrapper[prev].style = 'transform: translate(0, 0); opacity: 0;' }, 2000)
     this.carouselWrapper[current].setAttribute('data-current', '1')
   },
 }
