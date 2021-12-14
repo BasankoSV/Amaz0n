d38dcbcb2cc08ca9f3570d0b7f08e2df664adb3a
BasankoSV <basankosv@gmail.com>
2021-12-14 08:00:55 +0500

    Button disabled, duration: 1.5s

--- Amaz0n/js/script.js
+++ Amaz0n/js/script.js
@@ -9,6 +9,11 @@
     carouselWrapper: document.querySelectorAll('.carousel-wrapper'),
 
     currentChange(direction) {
+      const btn = event.currentTarget;
+      btn.setAttribute('disabled', 'disabled');
+      setTimeout(() => {
+        btn.removeAttribute('disabled');
+      }, 2000);
       let current;
 
       for (let i = 0; i < this.carouselWrapper.length; i++) {
@@ -20,14 +25,14 @@
 
       switch (direction) {
         case 'left':
-          this.carouselWrapper[current].style = 'transform: translate(120%, 0); transition: transform 1s ease 1s;';
+          this.carouselWrapper[current].style = 'transform: translate(120%, 0); transition: transform 1.5s ease 1s;';
           current--;
           current < 0 ? current = this.carouselWrapper.length - 1 : current;
           this.carouselWrapper[current].style = 'transform: translate(-120%, 0);';
           break;
 
         case 'right':
-          this.carouselWrapper[current].style = 'transform: translate(-120%, 0); transition: all 1s ease 1s';
+          this.carouselWrapper[current].style = 'transform: translate(-120%, 0); transition: all 1.5s ease 1s';
           current++;
           current === this.carouselWrapper.length ? current = 0 : current;
           this.carouselWrapper[current].style = 'transform: translate(120%, 0);';
@@ -35,7 +40,7 @@
       }
 
       setTimeout(() => {
-        this.carouselWrapper[current].style = 'transform: translate(0, 0); transition: all 1s ease 1s;';
+        this.carouselWrapper[current].style = 'transform: translate(0, 0); transition: all 1.5s ease 1s;';
       }, 1);
       setTimeout(() => {
         this.carouselWrapper[prev].style = 'transform: translate(0, 0); opacity: 0;';
